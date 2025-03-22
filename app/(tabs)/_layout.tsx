a62a36cfffe5cc54c93eae822d5be1e6f9c2b881
Lucas Maciel <maciellucas487@gmail.com>
2025-03-22 16:18:59 -0300

    feat/start

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,34 @@
+import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#03d3b7',
+      }}
+    >
+      {/* Tela Home */}
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      
+      {/* Tela About */}
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'information-circle-sharp' : 'information-circle-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
